421ae4390ddc7d63dba32f6e0213a7921e9866d8
itsdoanguen <doannguyennguyen654@gmail.com>
2025-04-15 14:22:12 +0700

    init prj

---
+++ api_web_learning_1/Data/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api_web_learning_1.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Models.TodoItem> TodoItems { get; set; } = null!;
+    }
+}
---
+++ api_web_learning_1/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace api_web_learning_1.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
